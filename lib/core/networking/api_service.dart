c5f9d699c08ebfd81d1a629bc34d04a0e7d4b646
Haidy <haidyahmedelsamman@gmail.com>
2024-08-30 04:43:47 +0300

    + addedt network files

---
+++ lib/core/networking/api_service.dart
@@ -0,0 +1,28 @@
+import 'package:dio/dio.dart';
+import 'package:news_app/core/networking/api_constants.dart';
+import 'package:retrofit/retrofit.dart';
+
+import '../../features/home/data/models/sources_response.dart';
+import '../../features/home/data/models/sources_title_response.dart';
+
+part 'api_service.g.dart';
+
+@RestApi(baseUrl: ApiConstants.apiBaseUrl)
+abstract class ApiService {
+  factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+
+  @GET(ApiConstants.everyThing)
+  Future<SourcesResponse> getArticals(
+    @Query('apikey') String apiKey,
+    @Query('q') String category,
+    @Query('sources') String source,
+  );
+
+  @GET(ApiConstants.sourcesTitle)
+  Future<SoursesTitleResponse> getSoursesTitle(
+    @Query('apikey') String apiKey,
+  );
+
+  
+
+}
